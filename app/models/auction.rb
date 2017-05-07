b0b1d0d1eb3cea3dc74a8e93d890548101d13f18
Rogier Nitschelm <rogier@invoicefinance.nl>
2017-05-07 19:34:15 +0200

    Slight change to validations

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,8 +1,8 @@
 class Auction < ApplicationRecord
   include SearchHelper
 
-  before_destroy :no_bids_present?
-  before_update :no_bids_present?
+  before_destroy :assert_no_bids_present
+  before_update :assert_no_bids_present
 
   has_many :bids, dependent: :destroy
   belongs_to :seller
@@ -12,7 +12,7 @@ class Auction < ApplicationRecord
   validates :starting_price, numericality: { greater_than: 0 }, allow_blank: false
   validates :buyout_price, numericality: { greater_than: :starting_price }, allow_blank: true
 
-  validate :not_in_the_past, :seller_is_verified?
+  validate :not_in_the_past?, :seller_is_verified?
 
   scope :active, -> { joins(:bids).where('bids.amount < auctions.buyout_price AND expires_at > ?', Time.zone.now) }
   scope :current_price, ->(minimum = 0, maximum = 9_999_999) {
@@ -29,7 +29,7 @@ class Auction < ApplicationRecord
     errors.add(:seller, 'is not verified') unless seller.verified?
   end
 
-  def not_in_the_past
+  def not_in_the_past?
     if !expires_at || expires_at < Time.zone.now
       errors.add(:expires_at, 'cannot be in the past or empty')
     end
@@ -41,7 +41,7 @@ class Auction < ApplicationRecord
     bids.order('amount DESC').first.amount
   end
 
-  def no_bids_present?
+  def assert_no_bids_present
     throw :abort unless bids.empty?
   end
 end
